adc817744d090b1e0386350473c15b39cb8d4788
iiww_B <iiww123@naver.com>
2024-01-28 12:41:14 +0900

    학교에서  제작한 로그인 프론트

--- server.js
+++ server.js
@@ -58,19 +58,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: true,
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-  resave: false,
+  name: 'user-cookies',
+  resave: true,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
 }))
 
 app.use(passport.initialize());
